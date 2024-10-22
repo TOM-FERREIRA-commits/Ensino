9aacd4be647c7a87ff7210a7daff32ff0fb8bbe7
TOM-FERREIRA-commits <loboDEV95@gmail.com>
2024-10-22 20:48:59 -0300

    dados_pc_Fatec

---
+++ dados.js
@@ -0,0 +1,20 @@
+var nome = ("Caneta");
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var imposto = 1.5;
+var preco_Final = preco + imposto;
+
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco_Final);
+
+nome = "Caneta BIC";
+console.log(nome);
+
+
+
+
