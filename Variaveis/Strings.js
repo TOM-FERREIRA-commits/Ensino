2279bb52b5e9d3b8f852c36c87ce42ee0394d77a
TOM-FERREIRA-commits <loboDEV95@gmail.com>
2024-10-29 20:40:07 -0300

    PC_FATEC

--- Variaveis/Strings.js
+++ Variaveis/Strings.js
@@ -1,3 +1,13 @@
 const escola = "Coder"
 
 console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))// lendo o codigo a partir do indice 1
+console.log(escola.substring(0, 3))// vai do indice 0 ao 3 
+console.log( 'Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3 ,'e')) 
+
+console.log('Ana, Maria, Pedro'.split(',')) // CRia um Array
