1c9dc2334898dd950ee661ddbf53c00d31ed0d49
TOM-FERREIRA-commits <loboDEV95@gmail.com>
2024-10-31 21:55:35 -0300

    Pc fatec

---
+++ Variaveis/UsandoVarEmLoop.js
@@ -0,0 +1,35 @@
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+console.log('i = ',i)
+
+//Usando Var em loop #2
+const funcs = []
+for (var i = 0; i < 10; i++){
+    funcs.push(function() {
+     console.log(i )   
+    })
+}
+
+funcs[2]()
+funcs[8]()
+
+
+
+// Usando let em loop
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+console.log('i = ',i)
+
+//Usando Let em loop#2
+
+const funcs = []
+for (let i = 0; i < 10; i++){
+    funcs.push(function() {
+     console.log(i )   
+    })
+}
+
+funcs[2]()
+funcs[8]()
