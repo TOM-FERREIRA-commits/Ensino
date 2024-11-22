5b87eb4b2b97e2ae7c3a66dd1b55cca6026cfda5
TOM-FERREIRA-commits <loboDEV95@gmail.com>
2024-11-21 21:04:17 -0300

    ATT

---
+++ Tarefa/Lista02/Concat.js
@@ -0,0 +1,11 @@
+function cumprimentar(nome) {
+  return `Olá, ${nome}!` // Retorna a mensagem formatada com o nome passado como argumento
+}
+
+// Exemplos de uso
+console.log(cumprimentar("Leonardo")) // "Olá, Leonardo!"
+console.log(cumprimentar("Maria"))    // "Olá, Maria!"
+
+
+
+
