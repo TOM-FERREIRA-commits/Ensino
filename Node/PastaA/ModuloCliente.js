2a03efe49078a37bcfd98f691bbef8a789be5ea9
TOM-FERREIRA-commits <tomdeveloper95@gmail.com>
2025-03-21 21:37:15 -0300

    teste

---
+++ Node/PastaA/ModuloCliente.js
@@ -0,0 +1,12 @@
+const ModuloA = require('./ModuloA');
+
+const moduloB = require('./ModuloB');
+
+console.log(ModuloA.ola);
+console.log(ModuloA.bemVindo);
+console.log(ModuloA.ateLogo);
+console.log(ModuloA);
+
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
+console.log(moduloB); 
---
+++ Node/PastaA/leituraArquivos.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+
+const caminho = __dirname + '/arquivo.json';    //__dirname é uma constante que está presente em todos os módulos do node que representa o diretório atual
+
+//sincrono
+
+const conteudo = fs.readFileSync(caminho, 'utf-8');
+console.log(conteudo);
+
+//assincrono
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo);
+    console.log(`${config.db.host}:${config.db.port}`);
+})
+
+const config = require('./arquivo.json'); //forma mais simples de ler um arquivo json
+console.log(config.db);
+
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteúdo da pasta...');
+    console.log(arquivos);
+})
